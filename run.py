6c98a619f84f11688667a8164a630c73f140c2b7
Pedro Bibiloni <p.bibiloni@uib.es>
2020-03-02 17:24:49 +0100

    Actualiza código de ejemplo

--- run.py
+++ run.py
@@ -12,10 +12,11 @@ def main():
     dcm = load_dcm(filename='16351644_s1_CT_PETCT.dcm')
 
     img_dcm = dcm.pixel_array
-    img_sagital = np.rot90(np.max(img_dcm, axis=2), k=-1)
+    img_sagital = np.rot90(img_dcm[:, :, 255], k=-1)
+    img_sagital_cmapped = plt.cm.get_cmap('bone')(img_sagital)
 
     [print(f'{k}: {v}') for k, v in dcm.items()]
-    plt.imshow(img_sagital, cmap=plt.cm.bone)
+    plt.imshow(img_sagital_cmapped, aspect=1)
     plt.show()
 
 
